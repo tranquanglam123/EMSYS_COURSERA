5250541a02a67d723e1d56e6afd14f372079ce79
tranquanglam123 <kwanglum815@gmail.com>
2022-09-11 14:40:06 +0700

    Finishing Assessment 2, compile native in src folder

--- src/sources.mk
+++ src/sources.mk
@@ -10,8 +10,11 @@
 #*****************************************************************************
 
 # Add your Source files to this variable
-SOURCES =
-
+SOURCES = \
+		./main.c\
+		./memory.c\
 # Add your include paths to this variable
-INCLUDES = 
+INCLUDES = -I ../include/common
+
+
 
